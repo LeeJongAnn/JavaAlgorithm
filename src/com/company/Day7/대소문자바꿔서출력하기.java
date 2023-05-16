9cf64c557488d14f6429290768db0a1b90271928
LeeJongAnn <dlwhddksiq@naver.com>
2023-05-16 23:46:13 +0900

    Day7 이어 붙인 수 추가 + 대소문자 바꿔서 출력하기

---
+++ src/com/company/Day7/대소문자바꿔서출력하기.java
@@ -0,0 +1,32 @@
+package com.company.Day7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class 대소문자바꿔서출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        String str = "";
+        char b;
+        List<Character> tt = new ArrayList<Character>();
+
+        for (int i = 0; i < a.length(); i++) {
+            b = a.charAt(i);
+            if (Character.isUpperCase(b)) {
+                b = Character.toLowerCase(b);
+                tt.add(b);
+            } else if (Character.isLowerCase(b)) {
+                b = Character.toUpperCase(b);
+                tt.add(b);
+            }
+        }
+
+        for (char list : tt) {
+            str += list;
+        }
+        System.out.println(str);
+    }
+}
