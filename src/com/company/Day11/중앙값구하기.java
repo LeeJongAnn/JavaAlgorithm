21307da5c92a51d4ba7ebd624d448c0eaa1853e0
LeeJongAnn <dlwhddksiq@naver.com>
2023-05-25 23:56:11 +0900

    Day11 중앙값 구하기 추가

---
+++ src/com/company/Day11/중앙값구하기.java
@@ -0,0 +1,22 @@
+package com.company.Day11;
+
+import java.util.Arrays;
+
+public class 중앙값구하기{
+
+    public static void main(String[] args) {
+
+        int a[] = {1, 2, 7, 10, 11};
+
+        solution(a);
+    }
+    public static int solution(int[] array) {
+        int a = 0;
+        Arrays.sort(array);
+        int result = (array.length / 2);
+        int returnResult = array[result];
+        System.out.println(returnResult);
+        return returnResult ;
+    }
+}
+
