a5019871331d96804021ed3748e6982fb5f69d86
LeeJongAnn <dlwhddksiq@naver.com>
2023-05-27 10:38:55 +0900

    Day13 가위 바위 보 ,문자열안에 문자열  추가

---
+++ src/com/company/Day13/가위바위보.java
@@ -0,0 +1,13 @@
+package com.company.Day13;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class 가위바위보 {
+    public String solution(String rsp) {
+        return Arrays.stream(rsp.split(""))
+                .map(s->s.equals("2")? "0" : s.equals("0") ? "5" : "2")
+                .collect(Collectors.joining());
+    }
+}
+
