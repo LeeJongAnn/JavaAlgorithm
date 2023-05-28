8fd2df82d794a51ec6597d54082cfc2f1fa75bf5
LeeJongAnn <dlwhddksiq@naver.com>
2023-05-28 15:59:03 +0900

    Day13 가위 바위 보 ,문자열안에 문자열  추가

---
+++ src/com/company/Day14/암호해독.java
@@ -0,0 +1,26 @@
+package com.company.Day14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 암호해독 {
+
+    public String solution(String cipher, int code) {
+        char str;
+        String result;
+        List<String> array = new ArrayList<String>();
+        for(int i = code-1; i<cipher.length(); i +=code) {
+
+            str = cipher.charAt(i);
+            result = Character.toString(str);
+            array.add(result);
+        }
+        String strResult = "";
+        for(String list : array) {
+            strResult += list;
+        }
+
+        return strResult;
+    }
+}
+
