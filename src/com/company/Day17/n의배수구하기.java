194bfd86af9b6a8cbe9028ef845f3cf3c3549947
LeeJongAnn <dlwhddksiq@naver.com>
2023-05-31 21:24:41 +0900

    Day17 n의 배수 구하기 , 문자열 정렬하기2 추가

---
+++ src/com/company/Day17/n의배수구하기.java
@@ -0,0 +1,21 @@
+package com.company.Day17;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class n의배수구하기 {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int[] numlist = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int n = 0;
+        solution(a, numlist);
+    }
+
+    public static int[] solution(int n, int[] numlist) {
+
+        return Arrays.stream(numlist).filter(s -> s % n == 0).toArray();
+    }
+}
+
