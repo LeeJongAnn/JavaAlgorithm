da133ad3d398895e34ab2985f4e18e268178d379
LeeJongAnn <dlwhddksiq@naver.com>
2023-06-03 21:42:53 +0900

    Day18 피자나눠먹기2 ,최댓값만들기2 추가

---
+++ src/com/company/Day18/피자나눠먹기2.java
@@ -0,0 +1,20 @@
+package com.company.Day18;
+
+import java.util.Arrays;
+
+public class 피자나눠먹기2 {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int[] numlist = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int n = 0;
+        solution(a, numlist);
+    }
+
+    public static int[] solution(int n, int[] numlist) {
+
+        return Arrays.stream(numlist).filter(s -> s % n == 0).toArray();
+    }
+}
+
