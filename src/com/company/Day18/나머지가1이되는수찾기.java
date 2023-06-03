a32e85e1abadd4b69fdfb105747d8ef5a81dba72
LeeJongAnn <dlwhddksiq@naver.com>
2023-06-03 22:40:26 +0900

    Day18 피자나눠먹기2 ,최댓값만들기2 첫_번째로_나오는_음수 추가 + 나머지가1이되는수찾기

---
+++ src/com/company/Day18/나머지가1이되는수찾기.java
@@ -0,0 +1,34 @@
+package com.company.Day18;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class 나머지가1이되는수찾기 {
+
+    public static void main(String[] args) {
+
+        solution(3);
+    }
+
+    public static int solution(int n) {
+
+        int result = 0;
+        List<Integer> array = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+
+            if (n % i == 1) {
+                System.out.println(i);
+                result = i;
+                array.add(result);
+            }
+        }
+
+        Collections.sort(array);
+        int last = array.get(0);
+        return last;
+    }
+}
+
