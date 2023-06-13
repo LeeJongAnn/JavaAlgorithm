4664c824209d62d590e9e374f9b247bf847d45ab
LeeJongAnn <dlwhddksiq@naver.com>
2023-06-13 21:37:04 +0900

    Day21 주사위의 개수 추가

---
+++ src/com/company/Day21/주사위의개수.java
@@ -0,0 +1,27 @@
+package com.company.Day21;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class 주사위의개수 {
+
+    public static void main(String[] args) {
+        int[] box = {10, 8, 6};
+        int n = 3;
+        solution(box, 3);
+    }
+    public static int solution(int[] box, int n) {
+
+        List<Integer> list = new ArrayList<>();
+        int match= 1;
+        int result = 1;
+        for(int a : box) {
+            result = a/n;
+            match *= result;
+            list.add(a);
+        }
+        System.out.println(list);
+        System.out.println(match);
+        return match;
+    }
+}
