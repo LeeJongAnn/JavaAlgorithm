51cac7b917c9450c56881843f66163aae2d9c5ad
LeeJongAnn <dlwhddksiq@naver.com>
2023-07-15 23:17:46 +0900

    [34.Day34 - 23년 7월 15일 토요일] | k의 개수 , 문자열내p와y의개수 제곱근판별,정수내림차순으로배치하기 추가

---
+++ src/com/company/Day33/정수내림차순으로배치하기.java
@@ -0,0 +1,26 @@
+package com.company.Day33;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class 정수내림차순으로배치하기 {
+
+    public static void main(String[] args) {
+        int n = 118372;
+        solution(n);
+    }
+
+    public static long solution(long n) {
+
+        String valueOf = String.valueOf(n);
+        List<String> collect = Arrays.stream(valueOf.split("")).sorted().collect(Collectors.toList());
+        Collections.reverse(collect);
+        System.out.println(collect);
+        String list = "";
+        for (String str : collect) {
+            list += str;
+        }
+        long parseLong = Long.parseLong(list);
+        return parseLong;
+    }
+}
