7f0e278e16be1a854f3300a9cb90833c618e21e0
LeeJongAnn <dlwhddksiq@naver.com>
2023-07-15 17:03:32 +0900

    [34.Day34 - 23년 7월 15일 토요일] | k의 개수

---
+++ src/com/company/Day33/k의개수.java
@@ -0,0 +1,34 @@
+package com.company.Day33;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+class k의개수 {
+
+    static int count = 0;
+    public static void main(String[] args) {
+        solution(10, 10, 0);
+    }
+
+    public static int solution(int i, int j, int k) {
+
+        List<Integer> integerArray = new ArrayList<>();
+
+        for (int z = i; z <= j; z++) {
+            integerArray.add(z);
+        }
+        List<String> collect2 = integerArray.stream().map(String::valueOf).collect(Collectors.toList());
+        for (String list : collect2) {
+            String[] split = list.split("");
+            for (String str : split) {
+                if (str.contains(String.valueOf(k))) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+}
