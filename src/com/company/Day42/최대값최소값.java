b5199516a6a77e3bba136e2f2f5c20cb0b9ef9ce
LeeJongAnn <dlwhddksiq@naver.com>
2023-08-07 21:29:48 +0900

    > | [43.Day43 - 23년 8월 7일 월요일]  | 문자열밀기 + 최대값최소값 추가

---
+++ src/com/company/Day42/최대값최소값.java
@@ -0,0 +1,33 @@
+package com.company.Day42;
+
+import com.company.Day39.Solution;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.til.stream.Collectors;
+
+class 최대값최소값 {
+
+    public static void main(String[] args) {
+        String s = "-1 -2 -3 -4";
+        Solution sol = new Solution();
+        sol.solution(s);
+    }
+
+    public String solution(String s) {
+        List<Integer> collect = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).boxed()
+                .collect(Collectors.toList());
+        Integer max = Collections.max(collect);
+        System.out.println(max);
+        Integer min = Collections.min(collect);
+        System.out.println(min);
+        String list = "";
+
+        list += min;
+        list += " ";
+        list += max;
+        System.out.println(list);
+        return list;
+    }
+}
