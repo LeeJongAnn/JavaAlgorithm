854a270e66a4bf001fd3cf7caa205bba6af58a8d
LeeJongAnn <dlwhddksiq@naver.com>
2023-08-16 22:27:31 +0900

    > | [46.Day46 - 23년 8월 16일 수요일] | 등수매기기 | + 코드 수정

--- src/com/company/Day45/등수매기기.java
+++ src/com/company/Day45/등수매기기.java
@@ -4,25 +4,33 @@ package com.company.Day45;
 import com.company.Day39.Solution;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 class 등수매기기 {
     public static void main(String[] args) {
-        int[] a = {1, 1, 2, 2, 7, 3, 3, 0, 1, 1};
+
+        int[][] score = { { 80, 70 }, { 90, 50 }, { 40, 70 }, { 50, 80 } };
         Solution sol = new Solution();
-        sol.solution(a);
+        sol.solution(score);
     }
 
-    public int[] solution(int[] arr) {
-        List<Integer> array = new ArrayList<Integer>();
-        array.add(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] != arr[i - 1]) {
-                array.add(arr[i]);
-            }
+    public int[] solution(int[][] score) {
+        List<Integer> array = new ArrayList<>();
+        List<Integer> copyArray = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            array.add(score[i][0] + score[i][1]);
         }
-        int[] array2 = array.stream().mapToInt(i -> i).toArray();
-        System.out.println(array2);
-        return array2;
+        int[] result = new int[score.length];
+        copyArray.addAll(array);
+        Collections.sort(copyArray, Comparator.reverseOrder());
+        for (int i = 0; i < array.size(); i++) {
+            result[i] = copyArray.indexOf(score[i][0] + score[i][1]);
+        }
+        for (int i = 0; i < result.length; i++) {
+            result[i] += 1;
+        }
+        return result;
     }
 }
