1903da59cd8b64512f704ea1152138f9376d270c
LeeJongAnn <dlwhddksiq@naver.com>
2023-09-14 19:56:06 +0900

    > | [53.Day53 - 23년 9월 14일 목요일] | 치킨쿠폰 |

---
+++ src/com/company/Day53/치킨쿠폰.java
@@ -0,0 +1,25 @@
+package com.company.Day53;
+
+import com.company.Day39.Solution;
+
+class 치킨쿠폰 {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.solution(1081);
+    }
+
+    public int solution(int chicken) {
+        int result = 0;
+        int coupon = chicken;
+        int serviceChickenSum = 0;
+
+        while (coupon >= 10) {
+            serviceChickenSum = coupon / 10;
+            coupon = coupon % 10 + serviceChickenSum;
+            result += serviceChickenSum;
+            System.out.println(result);
+        }
+        return serviceChickenSum;
+    }
+}
