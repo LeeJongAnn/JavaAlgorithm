55a82ec1d8d51010ef12b41d4c01e719514d4455
LeeJongAnn <dlwhddksiq@naver.com>
2023-10-07 18:45:11 +0900

    > | [61.Day60 - 23년 10월 7일 토요일] | 백준 - 수정렬하기 (2750번)

---
+++ src/com/company/baekjoon/bronze/Day1/배수와약수.java
@@ -0,0 +1,44 @@
+package com.company.baekjoon.bronze.Day1;
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class 배수와약수 {
+
+    static String result = "";
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int s1 = Integer.parseInt(st.nextToken());
+
+            if(s != 0 && s1 != 0) {
+
+                if (s1 % s == 0) {
+                    result = "factor";
+                    System.out.println(result);
+                } else if (s % s1 == 0) {
+                    result = "multiple";
+                    System.out.println(result);
+                } else {
+                    result = "neither";
+                    System.out.println(result);
+                }
+            } else {
+                break;
+            }
+        }
+
+    }
+
+}
---
+++ src/com/company/baekjoon/bronze/Day2/수정렬하기.java
@@ -0,0 +1,35 @@
+package com.company.baekjoon.bronze.Day2;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class 수정렬하기 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int num = Integer.parseInt(st.nextToken());
+
+        List<Integer> array = new ArrayList<>();
+
+        for(int i = 0; i<num; i++){
+            int result = Integer.parseInt(bf.readLine());
+            array.add(result);
+        }
+        Collections.sort(array);
+        for (int j = 0; j < num; j++) {
+            System.out.println(array.get(j));
+        }
+    }
+
+}
