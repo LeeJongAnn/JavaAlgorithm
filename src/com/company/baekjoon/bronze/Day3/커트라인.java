2c997ecb5baf88080c44696123a86da87f78b4a0
LeeJongAnn <dlwhddksiq@naver.com>
2023-10-08 09:30:34 +0900

    > | [63.Day61 - 23년 10월 8일 일요일] | 백준 - 커트라인 (25305번)

---
+++ src/com/company/baekjoon/bronze/Day3/커트라인.java
@@ -0,0 +1,33 @@
+package com.company.baekjoon.bronze.Day3;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class 커트라인 {
+
+    static int average = 0;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> array = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(bf.readLine());
+        for(int i = 0 ; i<num1; i++){
+            int score = Integer.parseInt(st.nextToken());
+            array.add(score);
+            Collections.sort(array,Comparator.reverseOrder());
+        }
+
+        int result = array.get(num2-1);
+        System.out.println(result);
+    }
+
+}
