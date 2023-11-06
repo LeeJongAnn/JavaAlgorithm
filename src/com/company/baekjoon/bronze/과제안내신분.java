02f2f47d84faa9331a3baaba98cab1097e08e1e4
LeeJongAnn <dlwhddksiq@naver.com>
2023-11-06 19:39:09 +0900

    > | [70.Day68 - 23년 11월 6일 토요일]  | 백준 - 과제 안내신분?

---
+++ src/com/company/baekjoon/bronze/과제안내신분.java
@@ -0,0 +1,35 @@
+package com.company.baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.*;
+
+public class 과제안내신분 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> array = new ArrayList<>();
+
+        for (int i = 0; i < 28; i++) {
+            int number = Integer.parseInt(bf.readLine());
+            array.add(number);
+        }
+
+        Collections.sort(array);
+
+        int[] newrray = new int[30];
+
+        for (int k = 1; k <= 30; k++) {
+
+            if (array.contains(k)) {
+                newrray[k-1] = k;
+            } else {
+                System.out.println(k);
+            }
+        }
+    }
+
+}
