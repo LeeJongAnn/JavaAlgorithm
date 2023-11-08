854b762d935316cc46ef2f2f9a2ad496fb58fb4a
LeeJongAnn <dlwhddksiq@naver.com>
2023-11-08 23:31:07 +0900

    > | [71.Day69 - 23년 11월 8일 토요일]  | 백준 - 피보나치수

---
+++ src/com/company/baekjoon/bronze/피보나치수.java
@@ -0,0 +1,30 @@
+package com.company.baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class 피보나치수 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int number = Integer.parseInt(bf.readLine());
+
+        System.out.println(fibo(number));
+    }
+
+    static int fibo(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+
+        }
+
+    }
+
+}
