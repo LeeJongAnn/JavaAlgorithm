acfcec7e2ff687bde5585c2b241e96292474751a
LeeJongAnn <dlwhddksiq@naver.com>
2023-11-11 20:12:00 +0900

    > | [74.Day72 - 23년 11월 11일 토요일] | 백준 - 주사위세개 + 직사각형탈출

---
+++ src/com/company/baekjoon/bronze/직사각형탈출.java
@@ -0,0 +1,38 @@
+package com.company.baekjoon.bronze;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.*;
+
+public class 직사각형탈출 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = bf.readLine();
+        String[] split = s.split(" ");
+        List<Integer> array = new ArrayList<>();
+
+        int x = Integer.parseInt(split[0]);
+        int y = Integer.parseInt(split[1]);
+        int w = Integer.parseInt(split[2]);
+        int h = Integer.parseInt(split[3]);
+
+        int num1 = x - 0;
+        int num2 = y - 0;
+
+        int num3 = w - x;
+        int num4 = h - y;
+
+        array.add(num1);
+        array.add(num2);
+        array.add(num3);
+        array.add(num4);
+
+        Integer min = Collections.min(array);
+        System.out.println(min);
+    }
+}
