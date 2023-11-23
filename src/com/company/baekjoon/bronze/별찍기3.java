c44b7f3ff550924b8da1f64e8b711c37cb1ae43d
LeeJongAnn <dlwhddksiq@naver.com>
2023-11-23 23:17:45 +0900

    > | [77.Day75 - 23년 11월 23일 목요일] | 백준 - 별찍기3

---
+++ src/com/company/baekjoon/bronze/별찍기3.java
@@ -0,0 +1,27 @@
+package com.company.baekjoon.bronze;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 별찍기3 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(bf.readLine());
+
+        for (int i = 1; i <= num; i++) {
+            for (int k = i; k <= num; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
