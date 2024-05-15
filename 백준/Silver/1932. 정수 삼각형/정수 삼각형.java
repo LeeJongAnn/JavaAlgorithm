b63508812cabb639c33937574f8702b6133e3a70
Lee Jong Ann <50771738+LeeJongAnn@users.noreply.github.com>
2024-05-15 09:57:26 +0900

    [Silver I] Title: 정수 삼각형, Time: 304 ms, Memory: 29356 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,26 +1,30 @@
 
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] arr;
-    static Integer dp[][];
+    static int arr[][];
 
-    static int depth;
+    static Integer dp[][];
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st;
+
 
         int N = Integer.parseInt(bf.readLine());
-
         dp = new Integer[N][N];
         arr = new int[N][N];
 
-        StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(bf.readLine(), " ");
             for (int j = 0; j < i + 1; j++) {
@@ -28,25 +32,26 @@ public class Main {
             }
         }
 
-
         for (int i = 0; i < N; i++) {
             dp[N - 1][i] = arr[N - 1][i];
         }
 
-        System.out.println(find(0, 0));
-
+        System.out.println(find(0,0));
     }
 
+
     static int find(int depth, int index) {
+
+        if (depth < 0) {
+            return 0;
+        }
 
         if (dp[depth][index] == null) {
             dp[depth][index] = Math.max(find(depth + 1, index), find(depth + 1, index + 1)) + arr[depth][index];
         }
 
-
         return dp[depth][index];
     }
-
 
 
 }
