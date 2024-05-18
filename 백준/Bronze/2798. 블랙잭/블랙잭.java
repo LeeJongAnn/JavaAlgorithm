5d45ab1e19d47629fcd1c6d7a344a8e089498ecf
Lee Jong Ann <50771738+LeeJongAnn@users.noreply.github.com>
2024-05-19 07:47:25 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,40 +1,39 @@
 
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    private static int array[];
+    static int arr[];
 
     public static void main(String[] args) throws IOException {
 
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = bf.readLine();
-        String[] split = s.split(" ");
-
+        String[] split = bf.readLine().split(" ");
 
         int N = Integer.parseInt(split[0]);
         int M = Integer.parseInt(split[1]);
 
-        array = new int[N];
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+
+        arr = new int[N];
+
         for (int i = 0; i < N; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         int temp = 0 ;
         int result = 0;
-
         for (int i = 0; i < N - 2; i++) {
             for (int j = i + 1; j < N - 1; j++) {
-                for (int k = j + 1; k< N; k++) {
-                    temp = array[i] + array[j] + array[k];
+                for (int k = j + 1; k < N; k++) {
+
+                    temp = arr[i] + arr[j] + arr[k];
+
                     if (temp == M) {
                         result = temp;
                     }
@@ -45,6 +44,11 @@ public class Main {
                 }
             }
         }
+
         System.out.println(result);
+
+
     }
+
+
 }
