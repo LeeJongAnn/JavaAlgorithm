c66f2faad728f54150f107c6f1812aad7bddbfc6
Lee Jong Ann <50771738+LeeJongAnn@users.noreply.github.com>
2024-05-18 09:42:50 +0900

    [Gold V] Title: LCS, Time: 208 ms, Memory: 29072 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,15 +1,16 @@
 
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-
-    static Integer dp[][];
-
     static char[] str1;
     static char[] str2;
+
+    static Integer dp[][];
 
     public static void main(String[] args) throws IOException {
 
@@ -18,33 +19,29 @@ public class Main {
         str1 = bf.readLine().toCharArray();
         str2 = bf.readLine().toCharArray();
 
+
         dp = new Integer[str1.length][str2.length];
 
-        System.out.println(LCS(str1.length - 1, str2.length - 1));
+
+        System.out.println(LCS(str1.length-1, str2.length-1));
+
 
     }
-
 
     static int LCS(int x, int y) {
 
-        if(x == -1 || y == -1) {
+        if (x == -1 || y == -1) {
             return 0;
         }
 
-        if(dp[x][y] == null) {
-            dp[x][y] = 0;
-
-            if(str1[x] == str2[y]) {
-                dp[x][y] = LCS(x - 1, y - 1) + 1;
-            }
-
-            else {
+        if (dp[x][y] == null) {
+            if (str1[x] == str2[y]) {
+                dp[x][y] = LCS(x-1,y-1) + 1;
+            } else {
                 dp[x][y] = Math.max(LCS(x - 1, y), LCS(x, y - 1));
             }
         }
-
         return dp[x][y];
     }
-
 
 }
